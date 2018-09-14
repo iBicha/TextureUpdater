bbe4b6403a11830414d4ae4499b65483de3b5d29
Brahim Hadriche <brahim.hadriche@gmail.com>
2018-09-14 14:27:09 -0600

    Example cleanup

--- Assets/TextureUpdater/Example/TextureUpdaterExample.cs
+++ Assets/TextureUpdater/Example/TextureUpdaterExample.cs
@@ -13,32 +13,31 @@ public class TextureUpdaterExample : MonoBehaviour
 
     private Color32[] colorBuffer;
 
-    private GCHandle handle;
-
-    // Use this for initialization
-    void Start()
+    private void Awake()
     {
         TextureUpdater.Init();
-
-        texture = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-        GetComponent<Renderer>().material.mainTexture = texture;
-        colorBuffer = new Color32[texture.width * texture.height];
-        handle = GCHandle.Alloc(colorBuffer, GCHandleType.Pinned);
-    }
-
-    void Update()
-    {
-        Plasma(colorBuffer, texture.width, texture.height);
-        TextureUpdater.Update(texture, handle.AddrOfPinnedObject());
     }
 
     private void OnDestroy()
     {
         TextureUpdater.Deinit();
-        handle.Free();
     }
 
-    void Plasma(Color32[] colors, int width, int height)
+    private void Start()
+    {
+        texture = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        GetComponent<Renderer>().material.mainTexture = texture;
+        colorBuffer = new Color32[texture.width * texture.height];
+    }
+
+
+    private void Update()
+    {
+        Plasma(colorBuffer, texture.width, texture.height);
+        TextureUpdater.Update(texture, colorBuffer);
+    }
+
+    private void Plasma(Color32[] colors, int width, int height)
     {
         var time = Time.time;
         for (var y = 0; y < height; y++)
@@ -46,7 +45,7 @@ public class TextureUpdaterExample : MonoBehaviour
             colors[y * width + x] = Plasma(x, y, width, height, time);
     }
 
-    Color32 Plasma(int x, int y, int width, int height, float time)
+    private Color32 Plasma(int x, int y, int width, int height, float time)
     {
         var px = (float) x / width;
         var py = (float) y / height;
@@ -58,10 +57,5 @@ public class TextureUpdaterExample : MonoBehaviour
         var b = (byte) (Mathf.Sin(l * 10) * 127 + 127);
 
         return new Color32(r, g, b, 255);
-    }
-
-    private Color32 GetRandomColor()
-    {
-        return new Color32((byte) Random.Range(0, 255), (byte) Random.Range(0, 255), (byte) Random.Range(0, 255), 255);
     }
 }
